ca076d57cd2abbfe40f5b01525434042d5412bda
Fatih Gumus <fatihgumus72@gmail.com>
2023-08-10 23:25:07 +0300

    added remove function from list

--- tableViewExample/ViewController.swift
+++ tableViewExample/ViewController.swift
@@ -7,12 +7,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    struct ImageType{
+        let image : String
+        let title : String
+    }
 
+    @IBOutlet weak var tableView: UITableView!
+    var superKahraman = [ImageType]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        superKahraman = [
+            ImageType(image: "batman", title: "Batman"),
+            ImageType(image: "superman", title: "Süperman"),
+            ImageType(image: "iron-man", title: "İron Man"),
+            ImageType(image: "captain-america", title: "Captain Amerika"),
+            ImageType(image: "spiderman", title: "Spiderman"),
+        
+        ]
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return superKahraman.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = superKahraman[indexPath.row].title;
+        
+        return cell
+    }
+    
+    func tableView(_ tableView :UITableView, commit editingStyle : UITableViewCell.EditingStyle,forRowAt indexPath: IndexPath){
+        
+        if editingStyle == .delete {
+            superKahraman.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.fade)
+        }
+    }
+    
+    
 
 
 }
